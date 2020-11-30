34a9ae976ea56dd870b9e431d5f734681401cc31
carawaters <cara_waters@icloud.com>
2020-11-30 11:41:09 +0000

    Creates histogram of background noise

--- hist.py
+++ hist.py
@@ -1,9 +1,41 @@
 import numpy as np
 from astropy.io import fits
+import matplotlib.pyplot as plt
+#from matplotlib.colors import LogNorm
+from scipy.optimize import curve_fit
+from scipy.stats import norm
 
 hdulist = fits.open('A1_mosaic.fits')
 
-print(hdulist[0].header)
-print(hdulist[0].data)
+header = hdulist[0].header
+data = hdulist[0].data
 
 hdulist.close()
+
+#remove high values
+data_small = data[data < 10000].flatten()
+
+#gaussian to fit
+def func(x, mu, std, a):
+    return a * np.exp(-np.power((x - mu), 2)/(2*std**2))
+
+plt.figure(1)
+#histogram with 5000 bins from testing
+n, bins, _ = plt.hist(data_small, bins=5000, color='orange')
+
+#generate bin centres
+points = []
+for i in range(1, len(bins)):
+    points.append(bins[i-1] + (bins[i]-bins[i-1])/2)
+
+popt, pcov = curve_fit(func, points, n, p0=[3410, 50, 5000])
+
+plt.plot(points, func(points, *popt), 'k', linewidth = 2, label = "Mean: " + str(int(round(popt[0], 0))) + " Std: " + str(int(round(popt[1], 0))))
+plt.xlim((3200, 3800))
+plt.xlabel('Pixel Value')
+plt.ylabel('Count')
+plt.legend()
+plt.grid()
+plt.savefig('noise_hist', dpi=300, bbox_inches='tight')
+
+plt.show()
