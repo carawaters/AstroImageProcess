Corrects radial masking of bright objects

masking.py
@@ -14,7 +14,7 @@ data = hdulist[0].data
 
 hdulist.close()
 
-data = data[3150:3350, 1250:2750]
+data = data[2250:4250, 500:2500]
 print(data)
 
 #detect objects by pixel value, min = mean + 4 std devs = 3467
@@ -26,7 +26,7 @@ def create_circular_mask(width, height, centre, radius):
     x, y = np.ogrid[:width, :height]
 
     dist_from_centre = np.sqrt((x - centre[0])**2 + (y - centre[1])**2)
-    mask = dist_from_centre >= radius
+    mask = dist_from_centre <= radius
 
     return mask
 
@@ -38,33 +38,37 @@ thresh = 20000
 #set up overall mask
 bright_points = data[data >= thresh]
 indices = np.nonzero(data >= thresh)
-current_mask = np.full_like(data, False, dtype=bool)
-print(current_mask)
+current_mask = np.array(np.logical_or(data >= val_max, data <= val_min), dtype=bool)
 print(len(bright_points))
+
+plt.figure(1)
+plt.imshow(current_mask, cmap='binary')
+plt.colorbar()
+ymin, ymax = plt.ylim()
+plt.ylim(ymax, ymin)
 
 #find points around brightest point to exclude
 for i in range(len(bright_points)):
     coord = (indices[0][i], indices[1][i])
     brightness = data[coord[0], coord[1]]
     j = 0
-    while brightness >= 35000:
-        if coord[0]+j == data.shape[0]:
+    while brightness >= val_min:
+        if coord[1]+j == data.shape[0]:
             break
         else:
-            brightness = data[coord[0]+j, coord[1]]
+            brightness = data[coord[0], coord[1]+j]
             j += 1
     circ = create_circular_mask(width, height, coord, j)
     current_mask = np.logical_or(current_mask, circ)
-    print(current_mask)
     print(i)
 
-plt.figure(1)
+plt.figure(2)
 plt.imshow(data, cmap = 'gray', norm=LogNorm())
 plt.colorbar()
 ymin, ymax = plt.ylim()
 plt.ylim(ymax, ymin)
 
-plt.figure(2)
+plt.figure(3)
 plt.imshow(current_mask, cmap='binary')
 plt.colorbar()
 ymin, ymax = plt.ylim()